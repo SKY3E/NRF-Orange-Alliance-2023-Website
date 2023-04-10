072e0213be3fff01aca4c2e9281b33463a16feac
SKY3E <bessin.raphael@gmail.com>
2023-04-09 21:49:36 -0400

    Next Project Template Edits - 4/9/23

---
+++ pages/apiView.js
@@ -0,0 +1,12 @@
+export default function apiView() {
+  return (
+    <main>
+      Api Viewer
+      <select>
+        <option value="Select Api View">Select Api View</option>
+        <option value="Team">Team</option>
+        <option value="Event">Event</option>
+      </select>
+    </main>
+  )
+}
