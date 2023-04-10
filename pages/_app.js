332e7e7b648bc8495086c2226b8290d89ce5d07b
SKY3E <bessin.raphael@gmail.com>
2023-04-09 22:03:54 -0400

    Firebase File Edits - 4/9/23

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,17 @@
+// Import UserContext and useUserData
+import { UserContext } from '../lib/context';
+import { useUserData } from '../lib/hooks';
+
+// Display website components and export it
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const userData = useUserData();
+  
+  return (
+    <main>
+      {/* Set UserContext to current user data */}
+      <UserContext.Provider value={ userData }>
+        <Component {...pageProps} />
+      </UserContext.Provider>
+    </main>
+  );
 }
