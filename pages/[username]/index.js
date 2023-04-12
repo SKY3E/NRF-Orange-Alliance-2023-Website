691a50b376269b759070a91f5cb9febdfc0a9e47
SKY3E <bessin.raphael@gmail.com>
2023-04-12 14:10:48 -0400

    Environment Variable Edits - 4/12/23

--- pages/[username]/index.js
+++ pages/[username]/index.js
@@ -28,27 +28,29 @@ export default function Profile({ user }) {
   return (
     <section className="ml-64 mt-20">
       <h1 className="text-3xl font-bold w-2/6 mb-2">Welcome back, {user.displayName}</h1>
-      <article className="rounded bg-blue-900 bg-opacity-50 p-2 mr-4 mb-2 text-white w-1/2">
-        <h2 className="text-xl">Account Info</h2>
-        <hr className="border-solid border-2 mb-2 mt-1"/>
-        <div className="flex">
-          <img className="rounded-full border-4 border-gray-100 border-opacity-30 h-16 w-16" src={user.photoURL || null} />
-          <div className="flex flex-col items-center justify-center ml-4">
-            <p>
-              <i>@{user.username}</i>
-            </p>
-            <h1>{user.displayName || 'Anonymous User'}</h1>
+      <div>
+        <article className="rounded bg-blue-900 bg-opacity-50 p-2 mr-4 mb-2 text-white w-1/2">
+          <h2 className="text-xl">Account Info</h2>
+          <hr className="border-solid border-2 mb-2 mt-1"/>
+          <div className="flex">
+            <img className="rounded-full border-4 border-gray-100 border-opacity-30 h-16 w-16" src={user.photoURL || null} />
+            <div className="flex flex-col items-center justify-center ml-4">
+              <p>
+                <i>@{user.username}</i>
+              </p>
+              <h1>{user.displayName || 'Anonymous User'}</h1>
+            </div>
           </div>
-        </div>
-      </article>
-      <article className="rounded bg-blue-900 bg-opacity-50 p-2 mr-4 mb-2 text-white w-1/2">
-        <h2 className="text-xl">Security Info</h2>
-        <hr className="border-solid border-2 mb-2 mt-1"/>
-        <div className="flex justify-between">
-          <Group />
-          <Authorization />
-        </div>
-      </article>
+        </article>
+        <article className="rounded bg-blue-900 bg-opacity-50 p-2 mr-4 mb-2 text-white w-1/2">
+          <h2 className="text-xl">Security Info</h2>
+          <hr className="border-solid border-2 mb-2 mt-1"/>
+          <div className="flex justify-between">
+            <Group />
+            <Authorization />
+          </div>
+        </article>
+      </div>
     </section>
   );
 }
