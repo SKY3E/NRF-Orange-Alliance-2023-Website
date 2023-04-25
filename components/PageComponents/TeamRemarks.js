927e03c75ff238fcbbcd70b998de93176f0a07ca
SKY3E <bessin.raphael@gmail.com>
2023-04-24 22:28:02 -0400

    Team Remark Display Edits - 4/24/23

--- components/PageComponents/TeamRemarks.js
+++ components/PageComponents/TeamRemarks.js
@@ -1,8 +1,8 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { getGroupWithUsername } from "@/lib/firebase";
 import { UserContext } from "../../lib/context";
 import { useContext } from "react";
-import { doc, updateDoc, setDoc, collection } from "firebase/firestore";
+import { doc, updateDoc, setDoc, collection, getDocs } from "firebase/firestore";
 import { firestore } from "../../lib/firebase";
 import { useRouter } from "next/router";
 
@@ -43,6 +43,29 @@ export default function TeamRemarks() {
   const router = useRouter();
   const { team } = router.query;
 
+  // Firestore fetchings functions
+  useEffect(() => {
+    getTeamRemarks();
+  }, [username, team, showTab]);
+
+  async function getTeamRemarks() {
+    const group = await getGroupWithUsername(username);
+    if (group == null) {
+      return 
+    } else {
+      const groupDoc = doc(firestore, "groups", group);
+      const groupTeamCollection = collection(groupDoc, group + "-TeamRemarks");
+      getDocs(groupTeamCollection)
+        .then((querySnapshot) => {
+          querySnapshot.forEach((doc) => {
+            if (doc.id == team) {
+              setText(doc.data().remarks);
+            }
+          });
+        })
+    }
+  }
+
   function handleTabButtonClick(event) {
     setShowTab(event.target.value);
   }
@@ -58,7 +81,7 @@ export default function TeamRemarks() {
       const groupDoc = doc(firestore, "groups", group);
       const groupTeamCollection = collection(groupDoc, group + "-TeamRemarks");
       const groupTeamRemarksDoc = doc(groupTeamCollection, team);
-      await setDoc(groupTeamRemarksDoc, { teamNumber: team, remarks: "" }, { merge: false })
+      await setDoc(groupTeamRemarksDoc, { teamNumber: team, remarks: "" }, { merge: false });
       updateDoc(groupTeamRemarksDoc, {
         remarks: text
       })
@@ -78,6 +101,7 @@ export default function TeamRemarks() {
           />
         </div>
         <hr className="border-solid border-blue-900 border-opacity-50 border-2 mb-2 mt-1" />
+        <p className="bg-white rounded text-black p-2 border-2 border-gray-300">{text}</p>
       </div>
     );
   }
