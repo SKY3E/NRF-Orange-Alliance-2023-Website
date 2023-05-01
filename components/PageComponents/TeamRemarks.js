7ae6e40cf14107049d15d2bfab1fecf2a6344d18
SKY3E <bessin.raphael@gmail.com>
2023-05-01 18:28:43 -0400

    Remark Edits - 5/1/23

--- components/PageComponents/TeamRemarks.js
+++ components/PageComponents/TeamRemarks.js
@@ -37,6 +37,7 @@ function ShowTabButton({ showTab, onTabButtonClick }) {
 export default function TeamRemarks() {
   const [showTab, setShowTab] = useState("View");
   const [text, setText] = useState("");
+  const [isGroup, setIsGroup] = useState(false);
   const { username } = useContext(UserContext);
 
   // Define router components
@@ -51,8 +52,9 @@ export default function TeamRemarks() {
   async function getTeamRemarks() {
     const group = await getGroupWithUsername(username);
     if (group == null) {
-      return 
+      setIsGroup(false);
     } else {
+      setIsGroup(true);
       const groupDoc = doc(firestore, "groups", group);
       const groupTeamCollection = collection(groupDoc, group + "-TeamRemarks");
       getDocs(groupTeamCollection)
@@ -90,45 +92,72 @@ export default function TeamRemarks() {
     }
   }
 
-  if (showTab == "View") {
+  if (isGroup) {
+    if (showTab == "View") {
+      return (
+        <div>
+          <div className="flex justify-between items-center">
+            <h2 className="text-xl">Team Remarks</h2>
+            <ShowTabButton
+              showTab={showTab}
+              onTabButtonClick={handleTabButtonClick}
+            />
+          </div>
+          <hr className="border-solid border-blue-900 border-opacity-50 border-2 mb-2 mt-1" />
+          <p className="bg-white rounded text-black p-2 border-2 border-gray-300">{text}</p>
+        </div>
+      );
+    }
+    if (showTab == "Edit") {
+      return (
+        <div>
+          <div className="flex justify-between items-center">
+            <h2 className="text-xl">Team Remarks</h2>
+            <ShowTabButton
+              showTab={showTab}
+              onTabButtonClick={handleTabButtonClick}
+            />
+          </div>
+          <hr className="border-solid border-blue-900 border-opacity-50 border-2 mb-2 mt-1" />
+          <form onSubmit={handleTextAreaSubmit}>
+            <textarea
+              value={text}
+              onChange={handleTextAreaChange}
+              className="bg-white rounded text-black px-1 border-2 border-gray-300 resize-w-none w-full"
+            />
+            <button
+              type="submit"
+              className="bg-green-600 hover:bg-opacity-50 rounded h-8 w-full"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+      );
+    }
+  } else {
     return (
       <div>
         <div className="flex justify-between items-center">
           <h2 className="text-xl">Team Remarks</h2>
-          <ShowTabButton
-            showTab={showTab}
-            onTabButtonClick={handleTabButtonClick}
-          />
         </div>
         <hr className="border-solid border-blue-900 border-opacity-50 border-2 mb-2 mt-1" />
-        <p className="bg-white rounded text-black p-2 border-2 border-gray-300">{text}</p>
-      </div>
-    );
-  }
-  if (showTab == "Edit") {
-    return (
-      <div>
-        <div className="flex justify-between items-center">
-          <h2 className="text-xl">Team Remarks</h2>
-          <ShowTabButton
-            showTab={showTab}
-            onTabButtonClick={handleTabButtonClick}
-          />
-        </div>
-        <hr className="border-solid border-blue-900 border-opacity-50 border-2 mb-2 mt-1" />
-        <form onSubmit={handleTextAreaSubmit}>
-          <textarea
-            value={text}
-            onChange={handleTextAreaChange}
-            className="bg-white rounded text-black px-1 border-2 border-gray-300 resize-w-none w-full"
-          />
-          <button
-            type="submit"
-            className="bg-green-600 hover:bg-opacity-50 rounded h-8 w-full"
+        <div className="bg-white rounded text-black p-2 border-2 border-gray-300 flex items-center space-x-2">
+          <svg
+            className="h-6 w-6 text-blue-900 opacity-50"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
           >
-            Submit
-          </button>
-        </form>
+            <path
+              clipRule="evenodd"
+              fillRule="evenodd"
+              d="M9.401 3.003c1.155-2 4.043-2 5.197 0l7.355 12.748c1.154 2-.29 4.5-2.599 4.5H4.645c-2.309 0-3.752-2.5-2.598-4.5L9.4 3.003zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z"
+            />
+          </svg>
+          <p>You are not in a group - please join one in the profile tab to add remarks.</p>
+        </div>
       </div>
     );
   }
